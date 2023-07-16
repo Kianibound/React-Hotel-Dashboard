3a4b90784e15735ad288133687f58478ff3d997e
kianibound <kianibound@gmail.com>
2023-07-16 19:14:33 +0330

    add main routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Booking from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const AppStyle = styled.div`
-  background-color: orangered;
-  width: 100vw;
-  height: 100vh;
-`;
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <AppStyle>
-        <H1>Hotel Dashboard</H1>
-        <Button
-          variation="primary"
-          size="medium"
-          onClick={() => alert("Clicked Me!")}
-        >
-          Click Me
-        </Button>
-        <Button
-          variation="danger"
-          size="small"
-          onClick={() => alert("Clicked Them!")}
-        >
-          Click Them
-        </Button>
-      </AppStyle>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Booking />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
