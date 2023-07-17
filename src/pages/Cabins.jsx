5ff1cb6e5db20e59ff86bd2f4850b971748a1f3c
kianibound <kianibound@gmail.com>
2023-07-18 00:48:10 +0330

    add react-hook-form and add cabin form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   // useEffect(() => {
   //   getCabins().then((data) => console.log(data));
   // }, []);
@@ -16,6 +19,11 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((showForm) => !showForm)}>
+          Add New Cabin
+        </Button>
+
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
