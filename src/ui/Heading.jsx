fd9c9b2aa180845296fccca835e27c93419b360f
kianibound <kianibound@gmail.com>
2023-07-24 13:49:24 +0330

    add authentication feature and login comp

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -4,19 +4,26 @@ const Heading = styled.h1`
   ${(props) =>
     props.as === "h1" &&
     css`
-      font-size: 3rem;
+      font-size: 5rem;
     `}
   ${(props) =>
     props.as === "h2" &&
     css`
-      font: 2rem;
+      font: 4rem;
       font-weight: 600;
     `}
   ${(props) =>
     props.as === "h3" &&
     css`
-      font: 2rem;
+      font: 300.5;
       font-weight: 400;
+    `}
+  ${(props) =>
+    props.as === "h4" &&
+    css`
+      font-size: 3rem;
+      font-weight: 800;
+      text-align: center;
     `}
 `;
 
