055d6e9391f36e5bc1439a3e2d391f9de108dc08
kianibound <kianibound@gmail.com>
2023-07-30 14:46:01 +0330

    add dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today Activities</div>
+      <div>Chart stay duration</div>
+      <div>Chart Sales</div>
+    </StyledDashboardLayout>
+  );
+}
