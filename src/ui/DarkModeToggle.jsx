b92de7040dbadd86ee74458c92b3bc1401060922
kianibound <kianibound@gmail.com>
2023-07-30 14:38:06 +0330

    complete dark mode feature

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,11 +1,15 @@
+import { HiOutlineMoon, HiOutlineSun } from "react-icons/hi2";
 import ButtonIcon from "./ButtonIcon";
-import { HiOutlineMoon } from "react-icons/hi2";
-const DarkModeToggle = () => {
+import { useDarkMode } from "../context/DarkModeContext";
+
+function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+
   return (
-    <ButtonIcon>
-      <HiOutlineMoon />
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
     </ButtonIcon>
   );
-};
+}
 
 export default DarkModeToggle;
