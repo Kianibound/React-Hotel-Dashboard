903e8eb25c2c4b6eee031007923d6603abf3ddb7
kianibound <kianibound@gmail.com>
2023-08-06 14:05:47 +0330

    add useRecentBookings and useRecentStays hooks for stats

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,19 +1,34 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: auto 34rem auto;
+	gap: 2.4rem;
 `;
 
 export default function DashboardLayout() {
-  return (
-    <StyledDashboardLayout>
-      <div>Statistics</div>
-      <div>Today Activities</div>
-      <div>Chart stay duration</div>
-      <div>Chart Sales</div>
-    </StyledDashboardLayout>
-  );
+	const { isLoading: isLoadingRecentBookings, bookings } = useRecentBookings();
+
+	const {
+		isLoading: isLoadingSatysStatus,
+		stays,
+		connfirmedStays,
+	} = useRecentStays();
+
+	if (isLoadingRecentBookings || isLoadingSatysStatus) return <Spinner />;
+
+	console.log(bookings);
+
+	return (
+		<StyledDashboardLayout>
+			<div>Statistics</div>
+			<div>Today Activities</div>
+			<div>Chart stay duration</div>
+			<div>Chart Sales</div>
+		</StyledDashboardLayout>
+	);
 }
